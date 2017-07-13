cf1552888e8ceeca1114eb3ff9354e2da26ccfde
RobBas666 <basson.robert@gmail.com>
2017-07-13 08:38:26 +0200

    old version

--- autotweet.php
+++ autotweet.php
@@ -1,19 +1,11 @@
 <?php
 	//$mins = $_REQUEST["interval"];
-	$Text=file('tokens.txt');
-	echo count($Text).'<br>';
-
-	/*$consumerKey = $Text[0];
-	//$consumerKey= 'f9iLv2YwAtAs7nlfkzldrjs31';
-	$consumerSecret = $Text[1];
-	//$consumerSecret = 'dbXvKs6oK5pkgVkJpz5K49JY2RwsbHu33KHtUava5M2adGbZ0e';
-	$accessToken = $Text[2];
-	//$accessToken = "884320356630228993-8xp43SZseryyphMhQloUmbVGCwscnJ1";
-	$accessTokenSecret = $Text[3];
-	//$accessTokenSecret =  "F0EKzvrwWMFfehl0jlippWcqdCRRFqDRQDfKw6VxmQK5D";*/
-    $conn = new mysqli("127.0.0.1", "bbd", "password","escape", 3306);
-
-	/*while (true){
+         $consumerKey = 'f9iLv2YwAtAs7nlfkzldrjs31';
+        $consumerSecret = 'dbXvKs6oK5pkgVkJpz5K49JY2RwsbHu33KHtUava5M2adGbZ0e';
+        $accessToken = '884320356630228993-8xp43SZseryyphMhQloUmbVGCwscnJ1';
+        $accessTokenSecret = 'F0EKzvrwWMFfehl0jlippWcqdCRRFqDRQDfKw6VxmQK5D';
+        $conn = new mysqli("127.0.0.1", "bbd", "password","Escape", 3306);
+	while (true){
 	for ($i = 1; $i <= 5; $i++){
         	//$sql = "SELECT COMMENTID, COMMENT, HANDLE FROM COMMENTS LEFT JOIN PRESENTER ON PRESENTER = PRESENTERID WHERE TWEETED = 0 AND TIME > (NOW() - interval 10 minute)  ORDER BY TIME DESC";
               $sql = "SELECT COMMENTID, COMMENT, HANDLE FROM COMMENTS LEFT JOIN PRESENTER ON PRESENTER = PRESENTERID WHERE TWEETED = 0 ORDER BY TIME DESC";
@@ -33,34 +25,26 @@
         	$track = fread($myfile, filesize("name.txt"));
 			echo $track;
 	        fclose($myfile);
-
 	        require_once('/codebird-php-develop/src/codebird.php');
-
         	 \Codebird\Codebird::setConsumerKey("$consumerKey", "$consumerSecret");
 	        $cb = \Codebird\Codebird::getInstance();
 	        $cb->setToken("$accessToken", "$accessTokenSecret");
 		$cb->setConnectionTimeout(10000);
 		$cb->setTimeout(15000);
-
 		if (sizeof($output) == 0) {$stat = "$track #BBDEscape";}
-	        else {$stat = " h $handle $track - $comment #BBDEscape";}
-
+	        else {$stat = "$handle $track - $comment #BBDEscape";}
 		if ($i == 1){
 			echo shell_exec('sh /var/www/cgi-bin/camera.sh');
         		$reply = $cb->media_upload(array(
         			'media' => '/home/pi/Desktop/image.jpg'
         		));
-
 	       		$mediaID = $reply->media_id_string;
 			$params = array('status' => $stat,'media_ids' => $mediaID);
 		}
 		else {$params = array('status' => $stat);}
-
 		$reply = $cb->statuses_update($params);
-
 	        $status = $reply->httpstatus;
         	echo "HTTP Response Code: $status";
-
 		$id = $output[0]["COMMENTID"];
 		if ($status == "200") {
 			$sql = "UPDATE COMMENTS SET TWEETED = 1 WHERE COMMENTID = $id";
@@ -68,5 +52,5 @@
 		}
 	}
 	sleep(300);
-	}*/
+	}
 ?>
